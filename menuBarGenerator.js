52c2c5c02f86483b595d41c92417ed2466570b84
Alex <alexjfuhr@gmail.com>
2021-09-28 13:48:58 -0400

    directory depth testing 2

--- menuBarGenerator.js
+++ menuBarGenerator.js
@@ -7,14 +7,14 @@ function makeList() {
       <a href="javascript:void(0)" class="dropbtn">Examples</a>
       <div class="dropdown-content">
         <a href="`+directoryDepth+`/Examples/Block_Level_Elements.html">Block Level Elements</a>
-        <a href="Examples/Desciptive_Lists.html">Descriptive Lists</a>
-        <a href="Examples/Getting_Down_With_HTML.html">Getting Down With HTML</a>
-        <a href="Examples/Inline_Elements.html">Inline Elements</a>
-        <a href="Examples/Learning_Web_Development.html">Learning Web Development</a>
-        <a href="Examples/Object_Elements.html">Object Elements</a>
-        <a href="Examples/Ordered_Lists.html">Ordered Lists</a>
-        <a href="Examples/Special_Characters.html">Special Characters</a>
-        <a href="Examples/Unordered_Lists.html">Unordered lists</a>
+        <a href="`+directoryDepth+`/Examples/Descriptive_Lists.html">Descriptive Lists</a>
+        <a href="`+directoryDepth+`/Examples/Getting_Down_With_HTML.html">Getting Down With HTML</a>
+        <a href="`+directoryDepth+`/Examples/Inline_Elements.html">Inline Elements</a>
+        <a href="`+directoryDepth+`/Examples/Learning_Web_Development.html">Learning Web Development</a>
+        <a href="`+directoryDepth+`/Examples/Object_Elements.html">Object Elements</a>
+        <a href="`+directoryDepth+`/Examples/Ordered_Lists.html">Ordered Lists</a>
+        <a href="`+directoryDepth+`/Examples/Special_Characters.html">Special Characters</a>
+        <a href="`+directoryDepth+`/Examples/Unordered_Lists.html">Unordered lists</a>
       </div>
     </li>`,
     `<li class="dropdown">
