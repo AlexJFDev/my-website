70aafa1b21e0a20d68feb6461f82f11d3f1519a8
Alex <alexjfuhr@gmail.com>
2021-09-28 14:51:57 -0400

    menu bar test2

--- menuBarGenerator.js
+++ menuBarGenerator.js
@@ -1,39 +1,7 @@
 function makeList() {
-  // Establish the array which acts as a data source for the list
   directoryDepth = ".".repeat(window.location.href.replace(/[^/A]/g, "").length-2)
-  let listData = [
-    `<a href="`+directoryDepth+`/index.html">Home</a>`,
-    `<li class="dropdown">
-      <a href="javascript:void(0)" class="dropbtn">Examples</a>
-      <div class="dropdown-content">
-        <a href="`+directoryDepth+`/Examples/Block_Level_Elements.html">Block Level Elements</a>
-        <a href="`+directoryDepth+`/Examples/Descriptive_Lists.html">Descriptive Lists</a>
-        <a href="`+directoryDepth+`/Examples/Getting_Down_With_HTML.html">Getting Down With HTML</a>
-        <a href="`+directoryDepth+`/Examples/Inline_Elements.html">Inline Elements</a>
-        <a href="`+directoryDepth+`/Examples/Learning_Web_Development.html">Learning Web Development</a>
-        <a href="`+directoryDepth+`/Examples/Object_Elements.html">Object Elements</a>
-        <a href="`+directoryDepth+`/Examples/Ordered_Lists.html">Ordered Lists</a>
-        <a href="`+directoryDepth+`/Examples/Special_Characters.html">Special Characters</a>
-        <a href="`+directoryDepth+`/Examples/Unordered_Lists.html">Unordered lists</a>
-      </div>
-    </li>`,
-    `<li class="dropdown">
-      <a href="javascript:void(0)" class="dropbtn">Templates</a>
-      <div class="dropdown-content">
-        <a href="`+directoryDepth+`/Templates/Template_Page.html">Template Page</a>
-      </div>
-    </li>`,
-    `<li class="dropdown">
-      <a href="javascript:void(0)" class="dropbtn">Important Links</a>
-      <div class="dropdown-content">
-        <a href="https://ict.gctaa.net/sections/webdesign/students.html">Other Websites</a>
-      </div>
-    </li>`
-  ],
-  // Make a container element for the list
-  listContainer = document.createElement('div'),
-  // Make the list
-  listElement = document.createElement('ul'),
+  listContainer = document.createElement('div');
+  listElement = document.createElement('ul');
   listElement.innerHTML =
   `<a href="`+directoryDepth+`/index.html">Home</a>
   <li class="dropdown">
