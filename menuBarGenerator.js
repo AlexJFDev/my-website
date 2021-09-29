582a846cea0b660937e0b550b9035003ae108152
Alex <alexjfuhr@gmail.com>
2021-09-29 08:20:15 -0400

    rearagned list

--- menuBarGenerator.js
+++ menuBarGenerator.js
@@ -8,14 +8,14 @@ listElement.innerHTML =
   <a href="javascript:void(0)" class="dropbtn">Examples</a>
   <div class="dropdown-content">
     <a href="`+directoryDepth+`/Examples/Block_Level_Elements.html">Block Level Elements</a>
-    <a href="`+directoryDepth+`/Examples/Descriptive_Lists.html">Descriptive Lists</a>
-    <a href="`+directoryDepth+`/Examples/Getting_Down_With_HTML.html">Getting Down With HTML</a>
-    <a href="`+directoryDepth+`/Examples/Inline_Elements.html">Inline Elements</a>
-    <a href="`+directoryDepth+`/Examples/Learning_Web_Development.html">Learning Web Development</a>
+    <a href="`+directoryDepth+`/Examples/Inline_Elements.html" style="background-color: rgb(255, 0, 255);">Inline Elements</a>
     <a href="`+directoryDepth+`/Examples/Object_Elements.html">Object Elements</a>
-    <a href="`+directoryDepth+`/Examples/Ordered_Lists.html">Ordered Lists</a>
     <a href="`+directoryDepth+`/Examples/Special_Characters.html">Special Characters</a>
+    <a href="`+directoryDepth+`/Examples/Descriptive_Lists.html">Descriptive Lists</a>
+    <a href="`+directoryDepth+`/Examples/Ordered_Lists.html">Ordered Lists</a>
     <a href="`+directoryDepth+`/Examples/Unordered_Lists.html">Unordered lists</a>
+    <a href="`+directoryDepth+`/Examples/Getting_Down_With_HTML.html">Getting Down With HTML</a>
+    <a href="`+directoryDepth+`/Examples/Learning_Web_Development.html">Learning Web Development</a>
   </div>
 </li>
 <li class="dropdown">
