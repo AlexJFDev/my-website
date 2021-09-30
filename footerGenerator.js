091d8b5863a8dd5afc86afbaa2c39dfc83b7b75c
Alex <alexjfuhr@gmail.com>
2021-09-30 09:15:36 -0400

    reverted footer

--- footerGenerator.js
+++ footerGenerator.js
@@ -1,12 +1,12 @@
 webPath = window.location.href;
+footerElement = document.createElement('ul');
+footerElement.style = "text-align: center;";
+footerElement.innerHTML =
+`<hr>
+<a href="http://validator.w3.org/check?uri=`+webPath+`">
+<strong> HTML </strong> Valid! </a> &nbsp;
+<a href="http://jigsaw.w3.org/css-validator/validator?uri=`+webPath+`?profile=css3">
+<strong> CSS </strong> Valid! </a>`;
 
-for (let i = 0; i < document.getElementsByClassName("verificationFooter").length; i++) {
-  document.getElementsByClassName("verificationFooter")[i].innerHTML = `
-  <footer>
-    <hr>
-    <a href="http://validator.w3.org/check?uri=`+webPath+`">
-    <strong> HTML </strong> Valid! </a> &nbsp;
-    <a href="http://jigsaw.w3.org/css-validator/validator?uri=`+webPath+`?profile=css3">
-    <strong> CSS </strong> Valid! </a>
-  </footer>`;
-}
+document.getElementsByTagName('body')[0].appendChild(footerElement);
+listContainer.appendChild(footerElement);
