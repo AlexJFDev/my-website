c1e6e8c093b794d50cb7531506884567340a7ead
Alex <alexjfuhr@gmail.com>
2021-09-30 14:36:52 -0400

    generated footer test

--- footerGenerator.js
+++ footerGenerator.js
@@ -1,8 +1,15 @@
-webPath = window.location.href;
-
-document.getElementById("generatedFooter").innerHTML =
+var url = window.location.pathname;
+var filename = url.substring(url.lastIndexOf('/')+1);
+filename = filename.replace(/_/g, ' ');
+var periodIndex = filename.indexOf('.')
+filename = filename.slice(0,periodIndex);
+for (let i = 0; i < document.getElementsByClassName("generatedFooter").length; i++) {
+  document.getElementsByClassName("generatedFooter")[i].innerHTML = filename;
+}
+/*
 `<hr>
 <a href="http://validator.w3.org/check?uri=`+webPath+`">
 <strong> HTML </strong> Valid! </a> &nbsp;
 <a href="http://jigsaw.w3.org/css-validator/validator?uri=`+webPath+`?profile=css3">
 <strong> CSS </strong> Valid! </a>`;
+*/
