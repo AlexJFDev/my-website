630d13a2718ebb867fdd43e8d7c4d55b35c55595
Alex <alexjfuhr@gmail.com>
2021-09-30 09:11:15 -0400

    div footer

--- footerGenerator.js
+++ footerGenerator.js
@@ -1,9 +1,12 @@
 webPath = window.location.href;
 
 for (let i = 0; i < document.getElementsByClassName("verificationFooter").length; i++) {
-  document.getElementsByClassName("verificationFooter")[i].innerHTML = `<hr>
-  <a href="http://validator.w3.org/check?uri=`+webPath+`">
-  <strong> HTML </strong> Valid! </a> &nbsp;
-  <a href="http://jigsaw.w3.org/css-validator/validator?uri=`+webPath+`?profile=css3">
-  <strong> CSS </strong> Valid! </a>`;
+  document.getElementsByClassName("verificationFooter")[i].innerHTML = `
+  <footer>
+    <hr>
+    <a href="http://validator.w3.org/check?uri=`+webPath+`">
+    <strong> HTML </strong> Valid! </a> &nbsp;
+    <a href="http://jigsaw.w3.org/css-validator/validator?uri=`+webPath+`?profile=css3">
+    <strong> CSS </strong> Valid! </a>
+  </footer>`;
 }
