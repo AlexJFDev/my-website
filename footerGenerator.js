3c60f2d07f523b8f136ea0cb7bd6d4d8bd8a1aa8
Alex <alexjfuhr@gmail.com>
2021-09-30 14:48:02 -0400

    footer test

--- footerGenerator.js
+++ footerGenerator.js
@@ -1,15 +1,14 @@
-var url = window.location.pathname;
-var filename = url.substring(url.lastIndexOf('/')+1);
-filename = filename.replace(/_/g, ' ');
-var periodIndex = filename.indexOf('.')
-filename = filename.slice(0,periodIndex);
-for (let i = 0; i < document.getElementsByClassName("generatedFooter").length; i++) {
-  document.getElementsByClassName("generatedFooter")[i].innerHTML = "<p>Footer</p>";
-}
-/*
+webPath = window.location.href;
+footerElement = document.createElement('ul');
+footerElement.style = "text-align: center;";
+footerElement.innerHTML =
 `<hr>
 <a href="http://validator.w3.org/check?uri=`+webPath+`">
 <strong> HTML </strong> Valid! </a> &nbsp;
 <a href="http://jigsaw.w3.org/css-validator/validator?uri=`+webPath+`?profile=css3">
 <strong> CSS </strong> Valid! </a>`;
-*/
+
+document.getElementsByTagName('body')[0].appendChild(footerElement);
+listContainer.appendChild(footerElement);
+
+document.getElementById("vLinks").innerHTML =
