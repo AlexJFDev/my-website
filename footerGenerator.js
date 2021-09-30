8d6d8f1380c4a80464ae7253c746e775b6353163
Alex <alexjfuhr@gmail.com>
2021-09-30 13:54:08 -0400

    footer generator

--- footerGenerator.js
+++ footerGenerator.js
@@ -1,12 +1,8 @@
 webPath = window.location.href;
-footerElement = document.createElement('ul');
-footerElement.style = "text-align: center;";
-footerElement.innerHTML =
+
+document.getElementById("generatedFooter").innerHTML =
 `<hr>
 <a href="http://validator.w3.org/check?uri=`+webPath+`">
 <strong> HTML </strong> Valid! </a> &nbsp;
 <a href="http://jigsaw.w3.org/css-validator/validator?uri=`+webPath+`?profile=css3">
 <strong> CSS </strong> Valid! </a>`;
-
-document.getElementsByTagName('body')[0].appendChild(footerElement);
-listContainer.appendChild(footerElement);
