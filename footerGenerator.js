ac6f44350efdc10532d9cf4f8816ca23557c9859
Alex <alexjfuhr@gmail.com>
2021-10-04 08:50:36 -0400

    fixed footer gen

--- footerGenerator.js
+++ footerGenerator.js
@@ -1,6 +1,6 @@
 var loc = window.location.href;
 
-var linkStr = '<a href="http://validator.w3.org/check?uri='+loc;+'">\n<strong> HTML </strong> Valid! </a> |'+' <a href="http://jigsaw.w3.org/css-validator/validator?uri='+loc+'?profile=css3">\n<strong> CSS </strong> Valid! </a>'
+var linkStr = '<a href="http://validator.w3.org/check?uri='+loc+'">\n<strong> HTML </strong> Valid! </a> |'+' <a href="http://jigsaw.w3.org/css-validator/validator?uri='+loc+'?profile=css3">\n<strong> CSS </strong> Valid! </a>'
 console.log(linkStr);
 
 var footer = document.getElementById("verifFooter");
