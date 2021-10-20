c4dfa36c978c18a4ce9347c27a765eadca00fdd9
Alex <alexjfuhr@gmail.com>
2021-10-20 09:18:02 -0400

    fully working javascript and updated CSS

--- green_house_gui.js
+++ green_house_gui.js
@@ -11,8 +11,34 @@ var hydroponic3 = {"daysSincePlanted": 35, "nutrients": "in limit", "image": "/i
 
 var ambientCdns = {"temperature": 72, "humidity": 67}
 
-var growBed1Element = document.getElementById("growBed1");
-console.log(growBed1Element);
-growBed1Element.innerHTML = `<td>`+growBed1["daysSincePlanted"]+` days</td>
-                             <td>`+growBed1["soilMoisture"]+` cb moisture</td>
-                             <td>`+growBed1["image"]+`</td>`;
+document.getElementById("growBed1").innerHTML = `<td>`+growBed1["daysSincePlanted"]+` days</td>
+                                                 <td>`+growBed1["soilMoisture"]+` cb moisture</td>
+                                                 <td><img src=`+growBed1["image"]+` style="width:50px;height:50px;"></td>`;
+document.getElementById("growBed2").innerHTML = `<td>`+growBed2["daysSincePlanted"]+` days</td>
+                                                 <td>`+growBed2["soilMoisture"]+` cb moisture</td>
+                                                 <td><img src=`+growBed2["image"]+` style="width:50px;height:50px;"></td>`;
+document.getElementById("growBed3").innerHTML = `<td>`+growBed3["daysSincePlanted"]+` days</td>
+                                                 <td>`+growBed3["soilMoisture"]+` cb moisture</td>
+                                                 <td><img src=`+growBed3["image"]+` style="width:50px;height:50px;"></td>`;
+document.getElementById("growBed4").innerHTML = `<td>`+growBed4["daysSincePlanted"]+` days</td>
+                                                 <td>`+growBed4["soilMoisture"]+` cb moisture</td>
+                                                 <td><img src=`+growBed4["image"]+` style="width:50px;height:50px;"></td>`;
+document.getElementById("growBed5").innerHTML = `<td>`+growBed5["daysSincePlanted"]+` days</td>
+                                                 <td>`+growBed5["soilMoisture"]+` cb moisture</td>
+                                                 <td><img src=`+growBed5["image"]+` style="width:50px;height:50px;"></td>`;
+document.getElementById("growBed6").innerHTML = `<td>`+growBed6["daysSincePlanted"]+` days</td>
+                                                 <td>`+growBed6["soilMoisture"]+` cb moisture</td>
+                                                 <td><img src=`+growBed6["image"]+` style="width:50px;height:50px;"></td>`;
+
+document.getElementById("hydroponic1").innerHTML = `<td>`+hydroponic1["daysSincePlanted"]+` days</td>
+                                                 <td>`+hydroponic1["soilMoisture"]+` cb moisture</td>
+                                                 <td><img src=`+hydroponic1["image"]+` style="width:50px;height:50px;"></td>`;
+document.getElementById("hydroponic2").innerHTML = `<td>`+hydroponic2["daysSincePlanted"]+` days</td>
+                                                 <td>`+hydroponic2["soilMoisture"]+` cb moisture</td>
+                                                 <td><img src=`+hydroponic2["image"]+` style="width:50px;height:50px;"></td>`;
+document.getElementById("hydroponic3").innerHTML = `<td>`+hydroponic3["daysSincePlanted"]+` days</td>
+                                                 <td>`+hydroponic3["soilMoisture"]+` cb moisture</td>
+                                                 <td><img src=`+hydroponic3["image"]+` style="width:50px;height:50px;"></td>`;
+
+document.getElementById("temperature").innerHTML = ambientCdns["temperature"]+` deg F`;
+document.getElementById("humidity").innerHTML = ambientCdns["humidity"]+`%`;
