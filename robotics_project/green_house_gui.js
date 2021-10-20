87e55c979948e816a927e667d12fffb2463b4cd4
Alex <alexjfuhr@gmail.com>
2021-10-20 13:29:47 -0400

    pictures

--- robotics_project/green_house_gui.js
+++ robotics_project/green_house_gui.js
@@ -16,29 +16,29 @@ document.getElementById("growBed1").innerHTML = `<td>`+growBed1["daysSincePlante
                                                  <td><img src=.`+growBed1["image"]+` style="width:50px;height:50px;"></td>`;
 document.getElementById("growBed2").innerHTML = `<td>`+growBed2["daysSincePlanted"]+` days</td>
                                                  <td>`+growBed2["soilMoisture"]+` cb moisture</td>
-                                                 <td><img src=`+growBed2["image"]+` style="width:50px;height:50px;"></td>`;
+                                                 <td><img src=.`+growBed2["image"]+` style="width:50px;height:50px;"></td>`;
 document.getElementById("growBed3").innerHTML = `<td>`+growBed3["daysSincePlanted"]+` days</td>
                                                  <td>`+growBed3["soilMoisture"]+` cb moisture</td>
-                                                 <td><img src=`+growBed3["image"]+` style="width:50px;height:50px;"></td>`;
+                                                 <td><img src=.`+growBed3["image"]+` style="width:50px;height:50px;"></td>`;
 document.getElementById("growBed4").innerHTML = `<td>`+growBed4["daysSincePlanted"]+` days</td>
                                                  <td>`+growBed4["soilMoisture"]+` cb moisture</td>
-                                                 <td><img src=`+growBed4["image"]+` style="width:50px;height:50px;"></td>`;
+                                                 <td><img src=.`+growBed4["image"]+` style="width:50px;height:50px;"></td>`;
 document.getElementById("growBed5").innerHTML = `<td>`+growBed5["daysSincePlanted"]+` days</td>
                                                  <td>`+growBed5["soilMoisture"]+` cb moisture</td>
-                                                 <td><img src=`+growBed5["image"]+` style="width:50px;height:50px;"></td>`;
+                                                 <td><img src=.`+growBed5["image"]+` style="width:50px;height:50px;"></td>`;
 document.getElementById("growBed6").innerHTML = `<td>`+growBed6["daysSincePlanted"]+` days</td>
                                                  <td>`+growBed6["soilMoisture"]+` cb moisture</td>
-                                                 <td><img src=`+growBed6["image"]+` style="width:50px;height:50px;"></td>`;
+                                                 <td><img src=.`+growBed6["image"]+` style="width:50px;height:50px;"></td>`;
 
 document.getElementById("hydroponic1").innerHTML = `<td>`+hydroponic1["daysSincePlanted"]+` days</td>
                                                  <td>`+hydroponic1["nutrients"]+`</td>
-                                                 <td><img src=`+hydroponic1["image"]+` style="width:50px;height:50px;"></td>`;
+                                                 <td><img src=.`+hydroponic1["image"]+` style="width:50px;height:50px;"></td>`;
 document.getElementById("hydroponic2").innerHTML = `<td>`+hydroponic2["daysSincePlanted"]+` days</td>
                                                  <td>`+hydroponic2["nutrients"]+`</td>
-                                                 <td><img src=`+hydroponic2["image"]+` style="width:50px;height:50px;"></td>`;
+                                                 <td><img src=.`+hydroponic2["image"]+` style="width:50px;height:50px;"></td>`;
 document.getElementById("hydroponic3").innerHTML = `<td>`+hydroponic3["daysSincePlanted"]+` days</td>
                                                  <td>`+hydroponic3["nutrients"]+`</td>
-                                                 <td><img src=`+hydroponic3["image"]+` style="width:50px;height:50px;"></td>`;
+                                                 <td><img src=.`+hydroponic3["image"]+` style="width:50px;height:50px;"></td>`;
 
 document.getElementById("temperature").innerHTML = ambientCdns["temperature"]+` deg F`;
 document.getElementById("humidity").innerHTML = ambientCdns["humidity"]+`%`;
