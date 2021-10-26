40480601d15e6725c98429c18fae248facb3eee1
Alex <alexjfuhr@gmail.com>
2021-10-26 08:16:17 -0400

    GDW

--- menuBarGenerator.js
+++ menuBarGenerator.js
@@ -3,17 +3,17 @@ directoryDepth = ".".repeat(window.location.href.replace(/[^/A]/g, "").length-2)
 var menuStr =
 `<li class="dropdownButton"><a href="`+directoryDepth+`/index.html">Home</a></li>
 <li class="dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Examples</a>
+  <a href="javascript:void(0)" class="dropbtn">GDW</a>
   <div class="dropdown-content">
-    <a href="`+directoryDepth+`/Examples/Block_Level_Elements.html">Block Level Elements</a>
-    <a href="`+directoryDepth+`/Examples/Inline_Elements.html">Inline Elements</a>
-    <a href="`+directoryDepth+`/Examples/Object_Elements.html">Object Elements</a>
-    <a href="`+directoryDepth+`/Examples/Special_Characters.html">Special Characters</a>
-    <a href="`+directoryDepth+`/Examples/Descriptive_Lists.html">Descriptive Lists</a>
-    <a href="`+directoryDepth+`/Examples/Ordered_Lists.html">Ordered Lists</a>
-    <a href="`+directoryDepth+`/Examples/Unordered_Lists.html">Unordered lists</a>
-    <a href="`+directoryDepth+`/Examples/Getting_Down_With_HTML.html">Getting Down With HTML</a>
-    <a href="`+directoryDepth+`/Examples/Learning_Web_Development.html">Learning Web Development</a>
+    <a href="`+directoryDepth+`/GDW/Block_Level_Elements.html">Block Level Elements</a>
+    <a href="`+directoryDepth+`/GDW/Inline_Elements.html">Inline Elements</a>
+    <a href="`+directoryDepth+`/GDW/Object_Elements.html">Object Elements</a>
+    <a href="`+directoryDepth+`/GDW/Special_Characters.html">Special Characters</a>
+    <a href="`+directoryDepth+`/GDW/Descriptive_Lists.html">Descriptive Lists</a>
+    <a href="`+directoryDepth+`/GDW/Ordered_Lists.html">Ordered Lists</a>
+    <a href="`+directoryDepth+`/GDW/Unordered_Lists.html">Unordered lists</a>
+    <a href="`+directoryDepth+`/GDW/Getting_Down_With_HTML.html">Getting Down With HTML</a>
+    <a href="`+directoryDepth+`/GDW/Learning_Web_Development.html">Learning Web Development</a>
   </div>
 </li>
 <li class="dropdown">
