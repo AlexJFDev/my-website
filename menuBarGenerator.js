a6014ff756c5f12f37e0331e9b52d149a097e854
Alex <alexjfuhr@gmail.com>
2021-10-26 08:35:35 -0400

    Added other subheding and padding test

--- menuBarGenerator.js
+++ menuBarGenerator.js
@@ -5,11 +5,12 @@ var menuStr =
 <li class="dropdown">
   <a href="javascript:void(0)" class="dropbtn">GDW</a>
   <div class="dropdown-content">
-    <p>Lists</p>
+    <p>Elements</p>
     <a href="`+directoryDepth+`/GDW/Block_Level_Elements.html">Block Level Elements</a>
     <a href="`+directoryDepth+`/GDW/Inline_Elements.html">Inline Elements</a>
     <a href="`+directoryDepth+`/GDW/Object_Elements.html">Object Elements</a>
     <a href="`+directoryDepth+`/GDW/Special_Characters.html">Special Characters</a>
+    <p>Lists</p>
     <a href="`+directoryDepth+`/GDW/Descriptive_Lists.html">Descriptive Lists</a>
     <a href="`+directoryDepth+`/GDW/Ordered_Lists.html">Ordered Lists</a>
     <a href="`+directoryDepth+`/GDW/Unordered_Lists.html">Unordered lists</a>
