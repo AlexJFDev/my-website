2d05c288994c335c08243533d54683112f045789
Alex <alexjfuhr@gmail.com>
2022-01-12 08:53:31 -0500

    Adding even more BEM

--- menuBarGenerator.js
+++ menuBarGenerator.js
@@ -3,8 +3,8 @@ directoryDepth = "../".repeat(window.location.href.replace(/[^/A]/g, "").length-
 var menuStr =
 `<li class="menu-bar__button"><a href="`+directoryDepth+`">Home</a></li>
 <li class="menu-bar__dropdown">
-  <a href="javascript:void(0)" class="dropbtn">GDW</a>
-  <div class="dropdown-content">
+  <a href="javascript:void(0)" class="menu-bar__dropdown__heading">GDW</a>
+  <div class="menu-bar__dropdown__content">
     <p>Elements</p>
     <a href="`+directoryDepth+`GDW/Block_Level_Elements.html">Block Level Elements</a>
     <a href="`+directoryDepth+`GDW/Inline_Elements.html">Inline Elements</a>
@@ -17,14 +17,14 @@ var menuStr =
   </div>
 </li>
 <li class="menu-bar__dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Templates</a>
-  <div class="dropdown-content">
+  <a href="javascript:void(0)" class="menu-bar__dropdown__heading">Templates</a>
+  <div class="menu-bar__dropdown__content">
     <a href="`+directoryDepth+`Templates/Template_Page.html">Template Page</a>
   </div>
 </li>
 <li class="menu-bar__dropdown">
-  <a href="javascript:void(0)" class="dropbtn">My Repos</a>
-  <div class="dropdown-content">
+  <a href="javascript:void(0)" class="menu-bar__dropdown__heading">My Repos</a>
+  <div class="menu-bar__dropdown__content">
     <p><a href="https://github.com/likebeingawesome">GitHub</a></p>
     <a href="https://github.com/likebeingawesome/likebeingawesome.github.io">This Website</a>
     <a href="https://github.com/likebeingawesome/Vanilla-Improvements-Agriculture">Vanilla Improvements Mod</a>
@@ -35,15 +35,15 @@ var menuStr =
   </div>
 </li>
 <li class="menu-bar__dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Java</a>
-  <div class="dropdown-content">
+  <a href="javascript:void(0)" class="menu-bar__dropdown__heading">Java</a>
+  <div class="menu-bar__dropdown__content">
     <a href="`+directoryDepth+`Java/Chap10/Java_Dates.html">Java Dates</a>
     <a href="`+directoryDepth+`Java/Midi/Java_Midi_Player.html">Java Midi Player</a>
   </div>
 </li>
 <li class="menu-bar__dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Robotics Projects</a>
-  <div class="dropdown-content">
+  <a href="javascript:void(0)" class="menu-bar__dropdown__heading">Robotics Projects</a>
+  <div class="menu-bar__dropdown__content">
     <a href="`+directoryDepth+`robotics_project/Green_House_GUI.html">Green House Project</a>
   </div>
 </li>`;
