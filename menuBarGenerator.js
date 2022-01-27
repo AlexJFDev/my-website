00ab0ff08c73f5975d0a8481b5b1bf0313cf6555
Alex <alexjfuhr@gmail.com>
2022-01-27 10:35:52 -0500

    Changed the menubar html string into a template string

--- menuBarGenerator.js
+++ menuBarGenerator.js
@@ -1,24 +1,24 @@
 directoryDepth = "../".repeat(window.location.href.replace(/[^/A]/g, "").length-3)
 
 var menuStr =
-`<li class="menu-bar__button"><a href="`+directoryDepth+`">Home</a></li>
+  `<li class="menu-bar__button"><a href="${directoryDepth}">Home</a></li>
 <li class="menu-bar__dropdown">
   <a href="javascript:void(0)" class="menu-bar__dropdown__heading">GDW</a>
   <div class="menu-bar__dropdown__content">
     <p class="menu-bar__dropdown__content--subheading">Elements</p>
-    <a href="`+directoryDepth+`GDW/Block_Level_Elements.html" class="menu-bar__dropdown__content--link">Block Level Elements</a>
-    <a href="`+directoryDepth+`GDW/Inline_Elements.html" class="menu-bar__dropdown__content--link">Inline Elements</a>
-    <a href="`+directoryDepth+`GDW/Object_Elements.html" class="menu-bar__dropdown__content--link">Object Elements</a>
-    <a href="`+directoryDepth+`GDW/Special_Characters.html" class="menu-bar__dropdown__content--link">Special Characters</a>
+    <a href="${directoryDepth}GDW/Block_Level_Elements.html" class="menu-bar__dropdown__content--link">Block Level Elements</a>
+    <a href="${directoryDepth}GDW/Inline_Elements.html" class="menu-bar__dropdown__content--link">Inline Elements</a>
+    <a href="${directoryDepth}GDW/Object_Elements.html" class="menu-bar__dropdown__content--link">Object Elements</a>
+    <a href="${directoryDepth}GDW/Special_Characters.html" class="menu-bar__dropdown__content--link">Special Characters</a>
     <p class="menu-bar__dropdown__content--subheading">Lists</p>
-    <a href="`+directoryDepth+`GDW/Ordered_Lists.html" class="menu-bar__dropdown__content--link">Ordered Lists</a>
-    <a href="`+directoryDepth+`GDW/Unordered_Lists.html" class="menu-bar__dropdown__content--link">Unordered lists</a>
+    <a href="${directoryDepth}GDW/Ordered_Lists.html" class="menu-bar__dropdown__content--link">Ordered Lists</a>
+    <a href="${directoryDepth}GDW/Unordered_Lists.html" class="menu-bar__dropdown__content--link">Unordered lists</a>
   </div>
 </li>
 <li class="menu-bar__dropdown">
   <a href="javascript:void(0)" class="menu-bar__dropdown__heading">Templates</a>
   <div class="menu-bar__dropdown__content">
-    <a href="`+directoryDepth+`Templates/Template_Page.html" class="menu-bar__dropdown__content--link">Template Page</a>
+    <a href="${directoryDepth}Templates/Template_Page.html" class="menu-bar__dropdown__content--link">Template Page</a>
   </div>
 </li>
 <li class="menu-bar__dropdown">
@@ -37,19 +37,19 @@ var menuStr =
 <li class="menu-bar__dropdown">
   <a href="javascript:void(0)" class="menu-bar__dropdown__heading">Java</a>
   <div class="menu-bar__dropdown__content">
-    <a href="`+directoryDepth+`Java/Chap10/Java_Dates.html" class="menu-bar__dropdown__content--link">Java Dates</a>
-    <a href="`+directoryDepth+`Java/Chap13/Pool_Puzzle.html" class="menu-bar__dropdown__content--link">Pool Puzzle</a>
-    <a href="`+directoryDepth+`Java/Midi/Java_Midi_Player.html" class="menu-bar__dropdown__content--link">Java Midi Player</a>
-    <a href="`+directoryDepth+`Java/Java_Particle_Simulation.html" class="menu-bar__dropdown__content--link">Java Midi Player</a>
+    <a href="${directoryDepth}Java/Chap10/Java_Dates.html" class="menu-bar__dropdown__content--link">Java Dates</a>
+    <a href="${directoryDepth}Java/Chap13/Pool_Puzzle.html" class="menu-bar__dropdown__content--link">Pool Puzzle</a>
+    <a href="${directoryDepth}Java/Midi/Java_Midi_Player.html" class="menu-bar__dropdown__content--link">Java Midi Player</a>
+    <a href="${directoryDepth}Java/Java_Particle_Simulation.html" class="menu-bar__dropdown__content--link">Java Midi Player</a>
   </div>
 </li>
 <li class="menu-bar__dropdown">
   <a href="javascript:void(0)" class="menu-bar__dropdown__heading">Robotics Projects</a>
   <div class="menu-bar__dropdown__content">
-    <a href="`+directoryDepth+`robotics_project/Green_House_GUI.html" target="_blank" class="menu-bar__dropdown__content--link">Green House Project</a>
+    <a href="${directoryDepth}robotics_project/Green_House_GUI.html" target="_blank" class="menu-bar__dropdown__content--link">Green House Project</a>
   </div>
 </li>
-<li class="menu-bar__button"><a href="`+directoryDepth+`resume/">Resume</a></li>`;
+<li class="menu-bar__button"><a href="${directoryDepth}resume/">Resume</a></li>`;
 console.log(menuStr);
 
 var menuList = document.getElementById("menu-bar");
