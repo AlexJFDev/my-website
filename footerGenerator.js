b1f0a72a5e6cf04acfbcd2e999395d50806f07cc
Alex <alexjfuhr@gmail.com>
2022-02-01 10:23:23 -0500

    Updated the verification links for the 404 page

--- footerGenerator.js
+++ footerGenerator.js
@@ -1,8 +1,8 @@
 var loc = window.location.href;
 
 var linkStr = `<hr><br>
-<a href="http://validator.w3.org/check?uri=`+loc+`" target="_blank">
-<strong> HTML </strong> Valid! </a> | <a href="http://jigsaw.w3.org/css-validator/validator?uri=`+loc+`?profile=css3" target="_blank">
+<a href="http://validator.w3.org/check?uri=${loc}" target="_blank">
+<strong> HTML </strong> Valid! </a> | <a href="http://jigsaw.w3.org/css-validator/validator?uri=${loc}?profile=css3" target="_blank">
 <strong> CSS </strong> Valid! </a>`;
 console.log(linkStr);
 
