a3474c6e53f32ea80928bcb5d83522d306334d25
Alex J. F <60758384+AlexJFDev@users.noreply.github.com>
2022-07-19 17:23:11 -0400

    Including blog 2

--- blog/sideBarGenerator.js
+++ blog/sideBarGenerator.js
@@ -6,6 +6,7 @@ var sideBarString =
 <p class="heading">Minecraft Modding</p>
 <ul class="sidebar-div--content">
   <li><a href="/blog/minecraft_modding/minecraft_modding_blog1">Modding Blog #1</a></li>
+  <li><a href="/blog/minecraft_modding/minecraft_modding_blog2">Modding Blog #2</a></li>
 </ul>
 </div>`;
 
