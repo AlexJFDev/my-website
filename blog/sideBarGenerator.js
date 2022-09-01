6fb592ef88e299a9e6c11b601b9125aa62ee2c4e
AlexJFDev <alexjfuhr@gmail.com>
2022-09-01 09:13:56 -0400

    Added a page for the first csc205 assignment

--- blog/sideBarGenerator.js
+++ blog/sideBarGenerator.js
@@ -1,17 +1,25 @@
 var sideBarString =
 `<div id ="home">
-<a href="/" class="heading">Home</a>
+  <a href="/" class="heading">Home</a>
+</div>
+<div id="csc205">
+  <p class="heading">CSC 205</p>
+  <ul>
+    <li><a href="/blog/csc205/the_octal_and_hexidecimal_number_systems">The Octal and Hexidecimal Number Systems</a></li>
+  </ul>
 </div>
 <div id ="modding">
-<p class="heading">Minecraft Modding</p>
-<ul class="sidebar-div--content">
-  <li><a href="/blog/minecraft_modding/minecraft_modding_blog1">Modding Blog #1</a></li>
-  <li><a href="/blog/minecraft_modding/minecraft_modding_blog2">Modding Blog #2</a></li>
-</ul>
-<p class="heading">Gardening</p>
-<ul>
-  <li><a href="/blog/gardening/gardening_blog1">Gardening Blog #1</a></li>
-</ul>
+  <p class="heading">Minecraft Modding</p>
+  <ul class="sidebar-div--content">
+    <li><a href="/blog/minecraft_modding/minecraft_modding_blog1">Modding Blog #1</a></li>
+    <li><a href="/blog/minecraft_modding/minecraft_modding_blog2">Modding Blog #2</a></li>
+  </ul>
+</div>
+<div id="gardening">
+  <p class="heading">Gardening</p>
+  <ul>
+    <li><a href="/blog/gardening/gardening_blog1">Gardening Blog #1</a></li>
+  </ul>
 </div>`;
 
 var sideBarElement = document.getElementById("sidebar");
