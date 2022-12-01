e8bee8575b9dbd6c3d757bba0ecb3f8f7b00b4bc
AlexJFDev <alexjfuhr@gmail.com>
2022-12-01 16:33:06 -0500

    Dropdown progress

--- footerGenerator.js
+++ footerGenerator.js
@@ -4,7 +4,6 @@ var linkStr = `<hr><br>
 <a href="http://validator.w3.org/check?uri=${loc}" target="_blank">
 <strong> HTML </strong> Valid! </a> | <a href="http://jigsaw.w3.org/css-validator/validator?uri=${loc}?profile=css3" target="_blank">
 <strong> CSS </strong> Valid! </a>`;
-console.log(linkStr);
 
 var footer = document.getElementById("footer-section");
 footer.innerHTML = linkStr;
